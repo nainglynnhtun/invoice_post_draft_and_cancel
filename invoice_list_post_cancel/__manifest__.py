869c603de05bd0783e435e5530d4b83fe89bdc54
nainglynnhtun <konainglynnhtun@gmail.com>
2023-11-15 14:32:27 +0700

    add 14

---
+++ invoice_list_post_cancel/__manifest__.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Invoice Post,Draft and Cancel from List View.",
+    'summary': """
+        Post your invoice/bill from list views and Multiple Cancel""",
+
+    'description': """
+        - Post, Reset to Draft, Register Payment and cancel customer invoice from list view
+        - Post, Reset to Draft, Register Payment and cancel vendor bill from list view
+    """,
+
+    'author': "Naing Lynn Htun",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Accounting/Accounting',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base','account'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/account_move.xml',
+    ],
+    'license': "AGPL-3",
+    'images': ['static/description/banner.png'],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+    
+}
